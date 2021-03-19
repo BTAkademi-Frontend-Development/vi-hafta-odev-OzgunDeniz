265aee15bbc2c5a9e060cdead20b519d0fb1e7ab
ozgunDeniz <ozgundenz@gmailcom>
2021-03-19 15:14:37 +0300

    js code has been added.

---
+++ js.js
@@ -0,0 +1,18 @@
+const programmer ={
+    name: "Özgün Deniz",
+    age : 25 ,
+    email : "ozgundenz@gmail.com",
+    
+    adress : {
+        city : "İstanbul",
+        street : "Kadıköy", //obje içinde obje bu şekilde yazılabilir.
+    },
+
+
+     work : function() {
+     console.log ("programmer is working..");},
+ 
+
+}
+
+console.log(programmer);
